a558510093909e5a282855bfe775af3de0b6e96a
Tingting Fang <ceci@tingtings-air.lan>
2021-09-28 21:42:44 -0700

    initial commit, web app connected to MySQL db

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, json
+import os
+import database.db_connector as db
+
+
+# Configuration
+
+app = Flask(__name__)
+
+db_connection = db.connect_to_database()
+
+
+# Routes 
+
+@app.route('/')
+def root():
+    return render_template("main.j2")
+
+@app.route('/bsg-people')
+def bsg_people():
+    query = "SELECT * FROM bsg_people"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("bsg.j2", bsg_people = results)
+
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 9112)) 
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
